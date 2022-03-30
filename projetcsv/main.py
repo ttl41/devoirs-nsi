f4c299b3ede974b45664c664a70fc45e6ee21b21
Beltza the Fox <24609727+beltzawoo@users.noreply.github.com>
2022-03-30 13:43:03 +0000

    aller jusqu'à l'exo 6

--- projetcsv/main.py
+++ projetcsv/main.py
@@ -1,4 +1,5 @@
 import csv
+from pprint import pprint
 from collections import OrderedDict
 
 def load_file(filename):
@@ -12,11 +13,11 @@ def load_file(filename):
 def display_list(liste, n=None, from_end=False):
   if n:
     if from_end:
-      print(liste[-n:])
+      pprint(liste[-n:])
     else:
-      print(liste[:n])
+      pprint(liste[:n])
   else:
-    print(liste)
+    pprint(liste)
 
 def filter_by_date(liste, une_seule_liste=False):
   if not une_seule_liste:
@@ -29,14 +30,71 @@ def filter_by_date(liste, une_seule_liste=False):
 
 def release_average(liste):
   release_years = [int(x['release_year']) for x in liste]
-  average = sum(release_years)/len(release_years)
+  average = round(sum(release_years)/len(release_years))
   return average
+
+def runtime_average(liste):
+  runtimes = [int(x['runtime']) for x in liste if x != 0]
+  average = round(sum(runtimes)/len(runtimes))
+  return average
+
+def runtime_average_by_year(liste):
+  runtimes_year = {}
+  for i in liste:
+    if i['runtime'] != '0':
+      try:
+        runtimes_year[i['release_year']].append(int(i['runtime']))
+      except KeyError:
+        runtimes_year[i['release_year']] = [int(i['runtime'])]
+  runtime_averages = {x[0]: round(sum(x[1])/len(x[1])) for x in runtimes_year.items()}
+  runtime_averages = list(runtime_averages.items())
+  runtime_averages.sort()
+  return runtime_averages
+      
+def count_runtimes(liste):
+  runtimes = [int(x['runtime']) for x in liste if x != 0]
+  short, medium, long, verylong = 0, 0, 0, 0
+  for i in runtimes:
+    if i <= 85:
+      short += 1
+    elif i  <= 120:
+      medium += 1
+    elif i <= 145:
+      long += 1
+    else:
+      verylong += 1
+  return [short, medium, long, verylong]
+
+def sort_movies(liste):
+  valid_list = [x for x in liste if x['revenue'] != '0']
+  sorted_movies = sorted(valid_list, key=lambda x: (x['release_year'], int(x['revenue'])))
+  return sorted_movies
+
+def two_best_movies(liste):
+  valid_list = [x for x in liste if x['revenue'] != '0']
+  profitable_movies = {}
+  for i in valid_list:
+    try:
+      if int(i['revenue']) > int(profitable_movies[i['release_year']][0]['revenue']):
+        profitable_movies[i['release_year']][0], profitable_movies[i['release_year']][1] = i, profitable_movies[i['release_year']][0],
+    except (KeyError, IndexError):
+      profitable_movies[i['release_year']] = [i, '']
+  profitable_movies = list(profitable_movies.items())
+  profitable_movies.sort()
+  return profitable_movies 
+    
+  
   
 def main():
   films, reals, distrib = map(load_file, ["films.csv", "realisateurs.csv", "distribution.csv"])
   print(f"Nombre d'enregistrements dans les fichiers films, réalisateurs, et distribution : {len(films)}, {len(reals)}, {len(distrib)}")
-  display_list(filter_by_date(films), 6)
-  print(release_average(films))
+  print("Année moyenne de sortie d'un film:", release_average(films))
+  print("Durée moyenne d'un film:", runtime_average(films), "minutes")
+  print("Nombre de films courts, moyen, longs, très longs:")
+  display_list(count_runtimes(films))
+  display_list(runtime_average_by_year(films))
+  display_list(sort_movies(films), 8, True)
+  display_list(two_best_movies(films))
 
 if __name__ == "__main__":
   main()
